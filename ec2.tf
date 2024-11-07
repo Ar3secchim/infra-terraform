23a030654493ae4c5adc16af107446ddc6913b86
Renara Secchim <renarasecchim@gmail.com>
2024-11-07 18:47:55 -0300

    refactor: :recycle: move security group definitions to a separate file

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,3 @@
-## SECURITY GROUPS 
-# security group for ec2 access
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow trafiic on port 80"
-  vpc_id      = aws_vpc.main.id
-
-  tags = {
-    Name = "allow_http"
-  }
-}
-
-# security group for acess http
-resource "aws_vpc_security_group_ingress_rule" "allow_http_rule" {
-  security_group_id = aws_security_group.allow_http.id
-
-  cidr_ipv4   = "10.0.0.0/16"
-  from_port   = 80
-  ip_protocol = "tcp"
-  to_port     = 80
-}
-
-
 # aws_instance ec2 zona a
 resource "aws_instance" "ec2-zona-a" {
   ami               = "ami-063d43db0594b521b"
