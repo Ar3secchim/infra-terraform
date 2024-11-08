3987c19564fedc5cf32e2525926530d7931f285d
Renara Secchim <renarasecchim@gmail.com>
2024-11-08 19:28:49 -0300

    feat: :sparkles: add egress rule for EC2 security group with appropriate tags

--- security_groups.tf
+++ security_groups.tf
@@ -18,4 +18,22 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_rule" {
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
+
+  tags = {
+    Name = "allow_http_rule"
+  }
+}
+
+#security group for ec2 egress
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.allow_http.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
+  to_port     = 0
+  from_port   = 0
+
+  tags = {
+    Name = "allow_all_egress"
+  }
 }
