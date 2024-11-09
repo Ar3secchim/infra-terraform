35ed6f9dff987d78106d6d5628dcdcbdc02e0e15
Renara Secchim <renarasecchim@gmail.com>
2024-11-09 18:19:08 -0300

    feat: :sparkles: update CloudFront policy version and simplify configuration in locals.tf

--- locals.tf
+++ locals.tf
@@ -4,27 +4,25 @@ data "aws_cloudfront_distribution" "current" {
   id = aws_cloudfront_distribution.s3_distribution.id
 }
 
+
 locals {
-  policy = jsonencode(
-    {
-      "Version" : "2008-10-17",
-      "Id" : "PolicyForCloudFrontPrivateContent",
-      "Statement" : [
-        {
-          "Sid" : "AllowCloudFrontServicePrincipal",
-          "Effect" : "Allow",
-          "Principal" : {
-            "Service" : "cloudfront.amazonaws.com"
-          },
-          "Action" : "s3:GetObject",
-          "Resource" : "arn:aws:s3:::${aws_s3_bucket.s3_bucket.bucket}/*",
-          "Condition" : {
-            "StringEquals" : {
-              "AWS:SourceArn" : "arn:aws:cloudfront::${data.aws_caller_identity.current.id}:distribution/${data.aws_cloudfront_distribution.current.id}"
-            }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontServicePrincipal",
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action   = "s3:GetObject",
+        Resource = "arn:aws:s3:::${aws_s3_bucket.s3_bucket.bucket}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.id}:distribution/${data.aws_cloudfront_distribution.current.id}"
           }
         }
-      ]
-    }
-  )
+      },
+    ]
+  })
 }
