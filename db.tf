e3bde9ab700252e0b223a8706805bf45d4b49d0e
Renara Secchim <renarasecchim@gmail.com>
2024-11-09 18:18:53 -0300

    feat: :sparkles: rename DB subnet group and update RDS instance configuration for improved performance

--- db.tf
+++ db.tf
@@ -1,6 +1,6 @@
 # DB SUBNET GROUP
-resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "db-subnet-group"
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
   subnet_ids = [aws_subnet.subnet-database-a.id, aws_subnet.subnet-database-b.id, aws_subnet.subnet-database-c.id]
   tags = {
     Name = "db-subnet-group"
@@ -9,16 +9,16 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 
 # DB INSTANCE 
 resource "aws_rds_cluster" "db_cluster" {
-  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
 
   engine             = "mysql"
   cluster_identifier = "db-cluster-mysql"
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
 
-  db_cluster_instance_class = "db.t3.micro"
-  storage_type              = "gp2"
-  allocated_storage         = 20
-  iops                      = 1000
+  db_cluster_instance_class = "db.c6gd.medium"
+  storage_type              = "io1"
+  allocated_storage         = 400
+  iops                      = 3000
 
   master_password = var.DB_PASSWORD
   master_username = var.DB_USERNAME
